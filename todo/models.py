2a42345c8a86761965ef5507fe9eb21e4e873a05
Alaa Dawood <adawood@cloudinn.net>
2021-10-02 23:34:20 +0200

    add a Task model , register it in admin site

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.db.models.base import Model
+class Task(models.Model):
+    TODO = 'T'
+    INPROGESSS = 'I'
+    DONE = 'D'
+    TASK_STATES = (
+        (TODO, 'To-DO'),
+        (INPROGESSS, 'In-progress'),
+        (DONE, 'Done'),
+    )
 
-    
+    title=models.CharField(max_length=300)
+    state=models.CharField(max_length=200,choices=TASK_STATES, default=TODO)
+    creation_date=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title
