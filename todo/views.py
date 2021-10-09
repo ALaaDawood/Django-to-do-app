3596d88f591c952bd7dfe6d1aeeb8a2ae3d12282
Alaa Dawood <adawood@cloudinn.net>
2021-10-10 01:23:15 +0200

    create template to show all tasks' info

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, HttpResponse
+from .models import Task
+def index(request):
+    tasks = Task.objects.all()
+    return render(request, 'index.html', {"tasks": tasks})
