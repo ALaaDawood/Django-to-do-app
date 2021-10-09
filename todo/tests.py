42c13c1c9858796062215975fda12ad4f8f64a34
Alaa Dawood <adawood@cloudinn.net>
2021-10-10 01:33:09 +0200

    firts trial: add first test case

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase
+from todo.models import Task
+
+class TaskTestCase(TestCase):
+    def setUp(self):
+        Task.objects.create(title="test first task with default state")
+        Task.objects.create(title="test task with state", state="I")
+
+    def test_task_default_state(self):
+        todo_task = Task.objects.get(title="test first task with default state")
+        in_progress_task = Task.objects.get(title="test task with state")
+        self.assertEqual(todo_task.get_state_display(), 'To-DO')
+        self.assertEqual(in_progress_task.get_state_display(), 'In-progress')
