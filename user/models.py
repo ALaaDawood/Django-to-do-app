ebeb2e6be282f42c1b06c787a72546a61cdc66bd
Alaa Dawood <adawood@cloudinn.net>
2022-01-08 16:24:33 +0200

    fix setting password for user

--- user/models.py
+++ user/models.py
@@ -94,7 +94,7 @@ class User(AbstractBaseUser, PermissionsMixin):
         return token
 
     def save(self, *args, **kwargs):
-        self.password = make_password(self.password)
+        self.set_password(self.password)
         super(User, self).save(*args, **kwargs)
 
     objects = UserManager()
